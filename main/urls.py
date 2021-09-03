8c039f72021c79f4777f2b1bd4cd774defbc59f7
Doruk Gezici <dorukgezici@gmail.com>
2021-09-04 01:32:42 +0300

    media files

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('portfolios/', include('portfolios.urls')),
     path('users/', include('users.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
