05eb599507b825adb5846dbffb8571b923c9a7a0
Doruk Gezici <dorukgezici@gmail.com>
2021-10-07 00:48:39 +0300

    New services deployed

--- backend/adiutor/urls.py
+++ backend/adiutor/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
+
+
+def health_check(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(status=200)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check),
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('adiutor.users.urls')),
     path('', include('adiutor.exchange.urls')),
