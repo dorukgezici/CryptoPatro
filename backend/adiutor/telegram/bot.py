a36e0a3d9ebfb19a57b4b483731dc7e5fb79d338
Doruk Gezici <dorukgezici@gmail.com>
2021-10-11 23:39:05 +0300

    bot fixed

--- backend/adiutor/telegram/bot.py
+++ backend/adiutor/telegram/bot.py
@@ -1,6 +1,6 @@
 import logging
+import os
 
-from django.conf import settings
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, Updater
 
@@ -9,11 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    user = update.effective_user
+    update.message.reply_text(f'Hello {user.first_name}. Your id is {user.id}.')
 
 
 if __name__ == '__main__':
-    updater = Updater(settings.TELEGRAM['bot_token'])
+    updater = Updater(os.environ.get('ADIUTOR_TELEGRAM_BOT_TOKEN'))
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.start_polling()
     updater.idle()
